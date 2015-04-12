55de70517d4ebf6f38b70fc3a4b0f7d4600fcf77
v0dk4l1m3 <marowen.ng@gmail.com>
2015-04-11 17:50:32 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+plot1 <- function() {
+## first download the data and extract the txt file into the working folder (in this case ~/R)
+## the next lines of codes are intended to read and extract the relevant data into a simpler table
+  setwd("~/R")
+  original <- read.delim("household_power_consumption.txt", sep = ";", header = TRUE)
+  original$Date <- as.Date(original$Date, "%d/%m/%Y")
+  extract <- subset(original, Date > "2007-01-31")
+  extract <- subset(extract, Date < "2007-02-03")
+  write.table(extract, file = "household.txt", sep = ";", col.names = TRUE, row.names = FALSE)
+  
+  table <- read.delim("household.txt", sep = ";", header = TRUE)
+
+## the next lines of codes are intended to create the first plot  
+  hist(as.numeric(table$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+}
